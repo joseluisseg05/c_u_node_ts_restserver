1cc7eecddef7d6ab1e4bd7e15fa446f97e6c9a75
Jose Luis Garcia M <jl.garcia@ardana.com.mx>
2021-03-18 17:22:03 -0600

    CRUD user

--- controller/usuarios.ts
+++ controller/usuarios.ts
@@ -1,46 +1,100 @@
 import { Request, Response } from "express";
+import Usuario from '../models/usuario';
 
-export const getUsuarios = ( req: Request, res: Response ) => {
-    
-    res.json({
-        msj: "getUsuarios"
+export const getUsuarios = async ( req: Request, res: Response ) => {
+    const usuarios = await Usuario.findAll({
+        where: {
+            estado: true
+        }
     });
+    res.json({ usuarios });
 }
 
-export const getUsuario = ( req: Request, res: Response ) => {
+export const getUsuario = async ( req: Request, res: Response ) => {
     const { id } = req.params;
+    const usuario = await Usuario.findByPk(id);
 
-    res.json({
-        msj: 'getUsuario',
-        id 
-    });
+    if ( usuario )
+        res.json({ usuario });
+    else
+        res.status(404).json({
+            msj: `No existe un usuario con el id ${id}`
+        })
 }
 
-export const postUsuario = ( req: Request, res: Response ) => {
+export const postUsuario = async ( req: Request, res: Response ) => {
     const { body } = req;
 
-    res.json({
-        msj: 'postUsuario',
-        body 
-    });
+    try {
+        const existeEmail = await Usuario.findOne({
+            where: {
+                email: body.email
+            }
+        });
+
+        if (existeEmail) {
+            return res.status(400).json({
+                msg: 'Ya existe un usuario con el email ' + body.email
+            });
+        }
+
+        const usuario = await Usuario.create(body);
+
+        res.json({ usuario })
+
+    } catch (error) {
+        console.log(error),
+        res.status(500).json({
+            msj: 'Error en el backend'
+        })
+    }
 }
 
-export const putUsuario = ( req: Request, res: Response ) => {
+export const putUsuario = async ( req: Request, res: Response ) => {
     const { id } = req.params;
     const { body } = req;
 
-    res.json({
-        msj: 'putUsuario',
-        body 
-    });
+    try {
+        
+
+        const usuario = await Usuario.findByPk( id );
+        if ( !usuario )
+            return res.status(404).json({
+                msj: 'No existe el usuario ' + id
+            })
+
+        await usuario.update( body );
+
+        res.json({ usuario })
+
+    } catch (error) {
+        console.log(error),
+        res.status(500).json({
+            msj: 'Error en el backend'
+        })
+    }
 }
 
-export const deleteUsuario = ( req: Request, res: Response ) => {
+export const deleteUsuario = async ( req: Request, res: Response ) => {
     const { id } = req.params;
-    res.json({
-        msj: 'deleteUsuario',
-        id
-    });
+
+    try {
+        const usuario = await Usuario.findByPk( id );
+        if ( !usuario )
+            return res.status(404).json({
+                msj: 'No existe el usuario ' + id
+            })
+
+        await usuario.update( {estado: false} );
+
+        res.json({ usuario })
+
+    } catch (error) {
+        console.log(error),
+        res.status(500).json({
+            msj: 'Error en el backend'
+        })
+    }
 }
 
 
